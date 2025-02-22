40d50f0c3b48683d340b0c0c1f7138628c45d4ba
David Bauer <david.bauer_@hotmail.com>
2025-02-22 19:44:19 -0300

    created build and config for the port 5500

---
+++ src/main/java/com/devsuperior/dslist/config/webConfig.java
@@ -0,0 +1,17 @@
+package com.devsuperior.dslist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
